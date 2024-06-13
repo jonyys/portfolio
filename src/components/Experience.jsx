b3f03b78ca467fe5e3bd1a95b6b409a0106407c0
jonyys <jnysg16@gmail.com>
2024-06-13 17:15:58 +0200

    update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,82 @@
-import { OrbitControls } from "@react-three/drei";
+import { Office } from "./Office";
+import { Avatar } from "./Avatar";
+import { motion } from "framer-motion-3d";
+import { animate, useMotionValue } from "framer-motion";
+import {
+  Float,
+  MeshDistortMaterial,
+  MeshWobbleMaterial,
+} from "@react-three/drei";
+import { BoxGeometry } from "three";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useEffect } from "react";
 
-export const Experience = () => {
+export const Experience = (props) => {
+  const { section } = props;
+  const { viewport } = useThree();
+
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <ambientLight intensity={1} />
+      <motion.group
+        position={[1.5, 2, 3]}
+        scale={[0.9, 0.9, 0.9]}
+        rotation-y={-Math.PI / 4}
+        animate={{
+          y: section === 0 ? 0 : -1,
+        }}
+      >
+        <Office section={section} />
+      </motion.group>
+
+      <motion.group
+        position={[0, -1.5, -10]}
+        animate={{
+          z: section === 1 ? 0 : -10,
+          y: section === 1 ? -viewport.height : -1.5,
+        }}
+      >
+        <directionalLight position={[-5, 3, 5]} intensity={0.4} />
+        <Float>
+          <mesh position={[1, -3, -15]} scale={[2, 2, 2]}>
+            <sphereGeometry />
+            <MeshDistortMaterial
+              opacity={0.8}
+              transparent
+              distort={0.4}
+              speed={4}
+              color={"red"}
+            />
+          </mesh>
+        </Float>
+        <Float>
+          <mesh scale={[3, 3, 3]} position={[3, 1, -18]}>
+            <sphereGeometry />
+            <MeshDistortMaterial
+              opacity={0.8}
+              transparent
+              distort={1}
+              speed={5}
+              color="yellow"
+            />
+          </mesh>
+        </Float>
+        <Float>
+          <mesh scale={[1.4, 1.4, 1.4]} position={[-3, -1, -11]}>
+            <boxGeometry />
+            <MeshWobbleMaterial
+              opacity={0.8}
+              transparent
+              factor={1}
+              speed={5}
+              color={"blue"}
+            />
+          </mesh>
+        </Float>
+        <group scale={[2, 2, 2]} position-y={-1.5}>
+          <Avatar animation={section === 0 ? "Falling" : "Standing"} />
+        </group>
+      </motion.group>
     </>
   );
 };
